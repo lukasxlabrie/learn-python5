6ea5ee5cdc95d7426ae62ff34a546c9c6cb846e5
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-07-30 15:52:42 -0400

    created print functions lines 9 - 14

--- ex5.py
+++ ex5.py
@@ -5,3 +5,10 @@ my_weight = 180 #lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
+
+print(f"Lets talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
